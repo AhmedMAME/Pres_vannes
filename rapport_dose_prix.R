a3aa8ec1e2bad2f08a782b93d0e95bc820a9eabf
PaulPer <perrotin.e1502310@etud.univ-ubs.fr>
2017-09-14 11:27:34 +0200

    rapport qualité prix medicament jour de traitement

--- rapport_dose_prix.R
+++ rapport_dose_prix.R
@@ -1,5 +1,7 @@
 library(ggplot2)
 
-df = read.csv2("./5.csv",header=T)
+df = read.csv2("./5.csv",dec=".",header = T)
+df$Prix_de_vente_2013 <- as.numeric(df$Prix_de_vente_2013)
 
-ggplot(data = df, aes(x=DDD_en_mg_par_jour,y=Prix_de_vente_2013))
+ggplot(data = df, aes(y=Nombre_de_jours_de_traitement,x=Prix_de_vente_2013,color=factor(Laboratoire)))+geom_point()+geom_bar(stat = "identity")
+
