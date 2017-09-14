156aff28d5f20be4b011c57d1186a2d4550fa705
Phil <phil.chart@hotmail.fr>
2017-09-14 14:36:59 +0200

    test avec gather (non fonctionnel)

--- Rapport Nb de vente - region.R
+++ Rapport Nb de vente - region.R
@@ -1,9 +1,17 @@
 library(ggplot2);
+library(tidyr)
+library(dplyr)
 
 df = read.csv2("./4.csv",header=T)
 
 head(df,n=10)
+Annee=""
+Valeur=""
+df = gather(Annee, Valeur, X2009:X2010)
 
-ggplot(data = df, aes(x=df[1,2:3], y=Region,color=factor(X2013)))+ geom_boxplot() + geom_point(position="jitter",alpha=0.3)
+head(df,n=10)
+
+#h = ggplot(data = df, aes(x=df$X2009, y=Region,color=factor(X2013)))+ geom_line()
+
 
 #hist(df$Region)
