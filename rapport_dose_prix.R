ff7b9b49800c6b7f1c27e21f6c273b3b1c8c71ee
PaulPer <perrotin.e1502310@etud.univ-ubs.fr>
2017-09-14 12:06:12 +0200

    pause de midi

--- rapport_dose_prix.R
+++ rapport_dose_prix.R
@@ -1,7 +1,6 @@
 library(ggplot2)
 
 df = read.csv2("./5.csv",dec=".",header = T)
-df$Prix_de_vente_2013 <- as.numeric(df$Prix_de_vente_2013)
-
-ggplot(data = df, aes(y=Nombre_de_jours_de_traitement,x=Prix_de_vente_2013,fill=factor(Laboratoire)))+geom_point()+geom_bar(stat = "identity")
+df$Prix_de_vente_2013 <- as.numeric(as.character(df$Prix_de_vente_2013))
+ggplot(data = df, aes(y=Nombre_de_jours_de_traitement,x=Prix_de_vente_2013,color=factor(Laboratoire)))+geom_point()
 
