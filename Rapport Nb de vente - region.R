27862d4c3b860edbd83505873a4432d18431f6b0
Phil <phil.chart@hotmail.fr>
2017-09-14 11:15:08 +0200

    feat: ajout chemin relatif

--- Rapport Nb de vente - region.R
+++ Rapport Nb de vente - region.R
@@ -1,10 +1,9 @@
 library(ggplot2);
 
-df = read.csv2("/Users/macbookpro/Documents/Cours3a/Master recherche/Methodologie/etude_donnee/Pres_vannes/4.csv",header=T)
+df = read.csv2("./4.csv",header=T)
 
 head(df,n=10)
 
+ggplot(data = df, aes(x=df[1,2:3], y=Region,color=factor(X2013)))+ geom_boxplot() + geom_point(position="jitter",alpha=0.3)
 
-#ggplot(data = df, aes(x=df[1,2:3], y=Region,color=factor(X2013)))+ geom_boxplot() + geom_point(position="jitter",alpha=0.3)
-
-hist(df$Region)
+#hist(df$Region)
